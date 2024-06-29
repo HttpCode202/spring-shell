b3b095366bff76d246b87f693d812ba584a22def
HttpCode202 <neant69neant89@gmail.com>
2024-06-29 15:42:06 +0200

    :sparkles: create project maven

---
+++ src/main/java/fr/httpcode202/spring_shell/SpringShellApplication.java
@@ -0,0 +1,13 @@
+package fr.httpcode202.spring_shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/httpcode202/spring_shell/SpringShellApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.httpcode202.spring_shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
